21bd39fec8c5a8c54a1f699c5a80ecd31aa7bc26
mayezra <may5ezra@gmail.com>
2025-10-26 14:01:28 +0200

    fix after terraform fmt -recursive

--- tf-workloads/production/main.tf
+++ tf-workloads/production/main.tf
@@ -1,6 +1,6 @@
 module "static_site" {
-  source      = "../../tf-modules/s3-static-website"
+  source = "../../tf-modules/s3-static-website"
 
   bucket_name = local.bucket_name
-  tags        = local.common_tags  
+  tags        = local.common_tags
 }
